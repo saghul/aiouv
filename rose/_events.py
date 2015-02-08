87d2d4348ffe096ee90cc87061d607b95b423230
Ludovic Gasc (GMLudo) <git@gmludo.eu>
2015-02-08 15:02:30 +0100

    Change close strategy because pyuv.Loop doesn't have anymore walk method

--- rose/_events.py
+++ rose/_events.py
@@ -94,13 +94,12 @@ class EventLoop(base_events.BaseEventLoop):
         self._ready.clear()
         self._timers.clear()
 
-        def cb(handle):
+        for handle in self._loop.handles:
             if not handle.closed:
                 handle.close()
-        self._loop.walk(cb)
 
         # Run a loop iteration so that close callbacks are called and resources are freed
-        self._loop.run(pyuv.UV_RUN_DEFAULT)
+        self._loop.run(pyuv.UV_RUN_ONCE)
         del self._loop._rose_loop
         self._loop = None
 
