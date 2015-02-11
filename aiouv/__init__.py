d37bea7579481b3baf3afd88e4f755846d6c2fe4
Ludovic Gasc (GMLudo) <git@gmludo.eu>
2015-02-12 00:04:56 +0100

    Change EventLoopPolicy heritage to reuse more code from AsyncIO: Two more tests passes in aiotest

--- aiouv/__init__.py
+++ aiouv/__init__.py
@@ -1,6 +1,5 @@
 
 import asyncio
-import threading
 
 from ._events import EventLoop
 from ._transports import connect_tcp, listen_tcp, connect_pipe, listen_pipe, create_udp_endpoint
@@ -11,20 +10,7 @@ __all__ = ['EventLoopPolicy', 'EventLoop',
            'create_udp_endpoint']
 
 
-class EventLoopPolicy(threading.local, asyncio.events.AbstractEventLoopPolicy):
+class EventLoopPolicy(asyncio.events.BaseDefaultEventLoopPolicy):
     """In this policy, each thread has its own event loop."""
 
-    _event_loop = None
-
-    def get_event_loop(self):
-        if self._event_loop is None:
-            self._event_loop = self.new_event_loop()
-        return self._event_loop
-
-    def set_event_loop(self, event_loop):
-        assert event_loop is None or isinstance(event_loop, asyncio.events.AbstractEventLoop)
-        self._event_loop = event_loop
-
-    def new_event_loop(self):
-        return EventLoop()
-
+    _loop_factory = EventLoop
